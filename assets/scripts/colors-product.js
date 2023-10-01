a92068501c41a3d43c5347f4a43d83d3d2ef11df
samuelwesleydev <114779480+samuelwesleydev@users.noreply.github.com>
2023-10-01 18:22:01 -0300

    update
    colocando imagens responsivas

--- assets/scripts/colors-product.js
+++ assets/scripts/colors-product.js
@@ -1,5 +1,6 @@
 export const ChangeColor = {
-    imgProduct: document.querySelectorAll('.img-product img'),
+    imgProduct: document.querySelectorAll('.img-product picture img'),
+    imgProductMobile: document.querySelectorAll('.img-product picture source'),
 
     colorNatural: document.querySelectorAll('.color.natural '),
     colorBlue: document.querySelectorAll('.color.blue '),
@@ -7,28 +8,19 @@ export const ChangeColor = {
     naturalColor(color,index){
         const pos = index
         seletedColor (color, index)
-
-        this.imgProduct.forEach((img, index)=>{
-            const indexImgEqualsIndex = index === pos
-            if(indexImgEqualsIndex) img.src = "assets/img/produtos/iphone-15-natural.jpeg"})
+        updateImg(color, index)
     },
 
     blueColor(color,index){
         const pos = index
         seletedColor (color, index)
-
-       this.imgProduct.forEach((img, index)=>{
-            const indexImgEqualsIndex = index === pos
-            if(indexImgEqualsIndex) img.src = "assets/img/produtos/iphone-15-darkblue.jpeg"})
+        updateImg(color, index)
     },
 
     blackColor(color,index){
         const pos = index
         seletedColor (color, index)
-
-        this.imgProduct.forEach((img, index)=>{
-            const indexImgEqualsIndex  = index === pos
-            if(indexImgEqualsIndex) img.src = "assets/img/produtos/iphone15-black.jpeg"})
+        updateImg(color, index)
     }
 
 }
@@ -38,6 +30,46 @@ function seletedColor (color, index){
     ChangeColor.colorBlue[index].classList.remove('selected')
     ChangeColor.colorNatural[index].classList.remove('selected')
     color.classList.add('selected')
+}
+
+function updateImg(color, index){
+    const button = color
+    const pos = index
+
+    switch (button.classList[1]) {
+        case 'natural':
+            ChangeColor.imgProductMobile.forEach((img, index)=>{
+            const indexImgEqualsIndex = index === pos
+            if(indexImgEqualsIndex) img.srcset = "assets/img/produtos/iphone-15-natural-mobile.jpeg"})
+
+            ChangeColor.imgProduct.forEach((img, index)=>{
+            const indexImgEqualsIndex = index === pos
+            if(indexImgEqualsIndex) img.src = "assets/img/produtos/iphone-15-natural.jpeg"})
+          break;
+
+        case 'black':
+            ChangeColor.imgProductMobile.forEach((img, index)=>{
+            const indexImgEqualsIndex = index === pos
+            if(indexImgEqualsIndex) img.srcset = "assets/img/produtos/iphone-15-black-mobile.jpeg"})
+
+            ChangeColor.imgProduct.forEach((img, index)=>{
+            const indexImgEqualsIndex = index === pos
+            if(indexImgEqualsIndex) img.src = "assets/img/produtos/iphone15-black.jpeg"})
+            break
+
+        case 'blue':
+            ChangeColor.imgProductMobile.forEach((img, index)=>{
+            const indexImgEqualsIndex = index === pos
+            if(indexImgEqualsIndex) img.srcset = "assets/img/produtos/iphone-15-darkblue-mobile.jpeg"})
+
+            ChangeColor.imgProduct.forEach((img, index)=>{
+            const indexImgEqualsIndex = index === pos
+            if(indexImgEqualsIndex) img.src = "assets/img/produtos/iphone-15-darkblue.jpeg"})
+          break;
+
+        default:
+          
+      }
 }
 
 
